32a0c38dab1b349a6aee7bf1e58745c21c6b18a3
csuvikg <gaborcsuvik@gmail.com>
2021-08-22 23:47:37 +0200

    Finish cfn template, clean up

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+## Deployment
+```shell
+aws cloudformation update-stack --stack-name devops-challenge --capabilities CAPABILITY_IAM --template-body file://cfn-resources.yaml
+```
+
 ## Tasks
 
 - Create resources in CloudFormation:
@@ -26,3 +31,4 @@
 - Instead of storing the password in SSM Parameter Store, AWS Secrets Manager can even generate and attach password to
   the RDS instance. I feel like it is more appropriate and is a more common practice. Also, Parameter Store SecureString
   type is not supported in CloudFormation
+- The database-related tasks are not very clear, some clarification would have helped a lot
--- entrypoint.py
+++ entrypoint.py
@@ -1,36 +1,19 @@
+from os import environ
+
 import boto3
-from flask import Flask
 
-app = Flask(__name__)
-
-db_instance_name = ''
+ssm_client = boto3.client('ssm')
+db_password = ssm_client.get_parameter(Name=environ['SSM_PARAMETER_NAME'])['Parameter']['Value']
 
 rds_client = boto3.client('rds')
+dbs = rds_client.describe_db_instances(DBInstanceIdentifier=environ['DB_INSTANCE_NAME'])['DBInstances']
 
-dbs = rds_client.describe_db_instances(DBInstanceIdentifier=db_instance_name)
+for db in dbs:
+    print('** RDS Version **')
+    print(f"Engine: {db['Engine']} (v{db['EngineVersion']})")
 
-
-@app.route("/")
-def index():
-    return "<p>Hello, World!</p>"
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
-
-# - connects
-# to
-# RDS
-# - prints
-# connection
-# properties, RDS
-# version
-# - retrieves
-# credentials
-# from SSM
-
-
-# https://developer.okta.com/blog/2018/07/31/use-aws-cloudformation-to-automate-static-site-deployment-with-s3
-# https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/using-cfn-cli-package.html
-# https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-codecommit-repository-s3.html
-# https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-codecommit-repository.html
+    print('\n** Connection details **')
+    print(f"Host: {db['Endpoint']['Address']}")
+    print(f"Port: {db['Endpoint']['Port']}")
+    print(f"User: {db['MasterUsername']}")
+    print(f"Password: {db_password}")
